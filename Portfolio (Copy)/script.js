e417c499218f3f7454334d1da5fca6955299e1c8
LeeDux <lee.duxbury94@yahoo.com>
2025-02-17 12:11:56 +0000

    added animation to burger menu icon and further mobile responsive styling

--- Portfolio (Copy)/script.js
+++ Portfolio (Copy)/script.js
@@ -5,5 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     hamburger.addEventListener('click', function() {
       hamburger.classList.toggle('open');
       navLinks.classList.toggle('open');
+
+      const icon = hamburger.querySelector("i");
+        if (navLinks.classList.contains("open")) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times"); 
+        } else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars"); 
+        }
     });
   });
